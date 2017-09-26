744c4b98188e534f7d5fc737b3471b29403ca20f
user name <email-for-github>
2017-09-26 16:49:39 +0800

    first commit

---
+++ sh.sh
@@ -0,0 +1,9 @@
+#   …   or create a new repository on the command line
+
+echo "# ftp_server" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/jeanslee/ftp_server.git
+git push -u origin master
+git push
